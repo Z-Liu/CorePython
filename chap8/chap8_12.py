b6663d644496432b21b778176fa9e95ca47788d9
Z-liu <liuzhao@yeah.net>
2017-02-13 23:07:40 +0800

    20170213

--- chap8/chap8_12.py
+++ chap8/chap8_12.py
@@ -1,10 +1,13 @@
 #coding=utf-8
 '''
-Created on 2017Äê2ÔÂ10ÈÕ
+Created on 2017ï¿½ï¿½2ï¿½ï¿½10ï¿½ï¿½
 
 @author: zhao
 '''
 begin_value=int(raw_input("enter begin value"))
 end_value=int(raw_input("enter end value"))
-def print(value):
-    print value
+def print_format(value):
+    print "{0:2d}".format(value), " {0:06b}".format(value),\
+     " {0:2o}".format(value), " {0:2x}".format(value), chr(value)
+for value in range(begin_value, end_value+1):
+    print_format(value)
