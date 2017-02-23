02b90919e38f69266bc9ab4ff4a43e36087cfade
Z-liu <liuzhao@yeah.net>
2017-02-23 21:48:35 +0800

    20170223

--- chap11/chap11_11.py
+++ chap11/chap11_11.py
@@ -1,6 +1,18 @@
 #coding=utf-8
 '''
-Created on 2017Äê2ÔÂ21ÈÕ
+Created on 2017ï¿½ï¿½2ï¿½ï¿½21ï¿½ï¿½
 
 @author: zhao
 '''
+def trim(line):
+    return line.strip(" ")
+f=open(raw_input("enter file"))
+new_lines=map(trim,f.readlines())
+def newfile():
+    file(f.name+"_new","w").writelines(new_lines)
+def overwrite():
+    file(f.name,"w").writelines(new_lines)
+selection={"1":newfile,
+           "2":overwrite}
+mode=selection[raw_input("select which way:1.newfile 2.overwrite")]
+mode()
